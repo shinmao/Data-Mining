42e6e02b2b68262d7cc43ecb4f20d1f8a8d912bf
Julia <julia@Juliade-MBP.fios-router.home>
2019-03-24 00:46:22 -0400

    ROC Curve and use random cv to select random forest  parameters

--- Data-Mining/hw2_teamwork/drug.py
+++ Data-Mining/hw2_teamwork/drug.py
@@ -3,22 +3,27 @@ import re
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold,RandomizedSearchCV,GridSearchCV
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 from imblearn.combine import SMOTETomek
-from imblearn.under_sampling import ClusterCentroids
+from imblearn.over_sampling import SMOTE
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn import tree
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, BaggingClassifier
 from sklearn.naive_bayes import GaussianNB
-from numpy  import array
+from numpy import array
+from scipy.stats import randint
+import random
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import KFold,StratifiedKFold
+from scipy import interp
+
 train_path = './train_drugs.data'
 test_path = './test.data'
 TRAIN_SIZE = 800
@@ -26,7 +31,7 @@ TEST_SIZE = 350
 # number of classifiers for cross validation to choose the best model
 # turned out that random forest might be the best model, set clf_num =1  
 clf_num = 1
-pca_lambda = 0.8
+pca_lambda = 0.85
 # number of estimaters for bagging and random forest classifiers
 num_trees = 100
 
@@ -44,27 +49,32 @@ def main():
     global train_set, test_set
     read_data()
     get_max_dimension()
-    train_set = sparse_to_dense(TRAIN_SIZE,train_data)
-    test_set = sparse_to_dense(TEST_SIZE,test_data)
+    train_set = revert_sparse(TRAIN_SIZE,train_data)
+    test_set = revert_sparse(TEST_SIZE,test_data)
+    
+    traindf = pd.DataFrame(train_set, dtype = 'float')
+    classdf = pd.DataFrame(cs)
     test_class=[]
+
     # split_data() only one time 
     # traindf, testdf,train_class,test_class = split_data()
-
-    # split_data() 10 times randomly, and do cross validation for each classifier
-    cross_validation()
+    
+    # perform K fold validation
+    # K_fold_validation(traindf, classdf)
 
     # create data frame for real testing
-    # traindf,testdf,train_class = create_dataframe()
+    traindf,testdf,train_class = create_dataframe()
+
     # resample data for real training
-    # test_red, train_resampled, class_resampled = training(traindf,testdf,train_class)
+    test_red, train_resampled, class_resampled = training_sampling(traindf,testdf,train_class)
 
     # classifiers
-    # random_forest_classifier(train_resampled,class_resampled,test_red,test_class)
-    # SVM_classifier(train_resampled,class_resampled,test_red)
+    random_forest_classifier(train_resampled,class_resampled,test_red,test_class)
+    # SVM_classifier(train_resampled,class_resampled,test_red,test_class)
     # decision_tree_classifier(train_resampled,class_resampled,test_red,test_class)
     # naive_bayes_classifier(train_resampled,class_resampled,test_red)
-    
-
+    # adaboost_classifier(train_resampled,class_resampled,test_red,test_class)    
+    # gradient_boosting_clasifier(train_resampled,class_resampled,test_red,test_class) 
 
 def read_data():
     # handle the train data into array 
@@ -84,7 +94,7 @@ def read_data():
         for col in range(6061):
             if col< len(train_buffer[row][1]):
                 train_data[row].append(train_buffer[row][1][col])
-
+    
     # cs[row] to show the class of row th
     for i in range(800):
         cs.append(int(train_buffer[i][0]))
@@ -109,7 +119,7 @@ def get_max_dimension():
         features.append(str(i))
 
 # convert sparse binary matrix to dense matrix
-def sparse_to_dense(rows,data):
+def revert_sparse(rows,data):
     dense_matrix = [[0]*(max_d+1) for i in range(rows)]
     for row in range(rows):
         for col in range(6061):
@@ -117,32 +127,101 @@ def sparse_to_dense(rows,data):
                     index = int(data[row][col])
                     dense_matrix[row][index] = 1
     return dense_matrix
-
-def cross_validation():
+# Perform K fold cross validation, cv = 10
+def K_fold_validation(traindf, train_class):
+    # Run classifier with cross-validation and plot ROC curves
+    skf = StratifiedKFold(n_splits=10,shuffle = False)
+    # balanced data
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    # imbalanced data
+    tprs_org = []
+    aucs_org = []
+    mean_fpr_org = np.linspace(0, 1, 100)
+    # arry for results
     cross_validation_res = np.zeros(shape=(clf_num,2))
+    cross_validation_res_imb = np.zeros(shape=(clf_num,2))
     f1_score_arr = np.zeros(shape=(clf_num,10,2))
-    
+    f1_score_arr_imb = np.zeros(shape=(clf_num,10,2))
+
     # list for each clasifier function 
     # classifier_functions=[SVM_classifier,decision_tree_classifier,naive_bayes_classifier,random_forest_classifier,\
     #     adaboost_classifier]
 
     classifier_functions=[random_forest_classifier]
     i = 0
-    while i<10:
-        # while doing the cross validation, the random state of split data need to be "None"s
-        traindf, testdf,train_class,test_class = split_data()
-        test_red, train_resampled, class_resampled = training(traindf,testdf,train_class)
-        # fill in f1 score of each classifier in the array
+    k=0
+    for train_index, test_index in skf.split(traindf,train_class):
+        # get data from dataframe
+        # data.iloc[[0,3,6,24], [0,5,6]] # 1st, 4th, 7th, 25th row + 1st 6th 7th columns.
+        X_train, X_test, y_train, y_test = traindf.iloc[train_index], traindf.iloc[test_index], train_class.iloc[train_index], train_class.iloc[test_index]
+        # Training each X_train (PCA and resample)
+        test_red, train_resampled, class_resampled = training_sampling(X_train,X_test,y_train)
+
+        # Loop for running different classifiers to select the best classifer for this dataset
         for j in range(len(classifier_functions)):
-            score = classifier_functions[j](train_resampled,class_resampled,test_red,test_class)
+            score, probas_ = classifier_functions[j](train_resampled,class_resampled,test_red,y_test)
             f1_score_arr[j][i]=score
+            
+            # roc curve
+            fpr, tpr, thresholds = roc_curve(y_test, probas_[:, 1])
+            tprs.append(interp(mean_fpr, fpr, tpr))
+            tprs[-1][0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            aucs.append(roc_auc)
         i+=1
+    
 
-    # get average f1 score of each classifier
-    for i in range(clf_num):
-        cross_validation_res[i] = f1_score_arr[i].mean(axis=0)
+        # use imbalance data to build the model
+        test_red, train_red, classes = training(X_train,X_test,y_train)
+        for j in range(len(classifier_functions)):
+            score, probas_org = classifier_functions[j](train_red,classes,test_red,y_test)
+            f1_score_arr_imb[j][k]=score
 
-    print(cross_validation_res)
+            # roc curve
+           
+            fpr, tpr, thresholds = roc_curve(y_test, probas_org[:, 1])
+            tprs_org.append(interp(mean_fpr_org, fpr, tpr))
+            tprs_org[-1][0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            aucs_org.append(roc_auc)
+        k+=1
+        # get average f1 score of each classifier
+    for l in range(clf_num):
+        cross_validation_res[l] = f1_score_arr[l].mean(axis=0)
+        cross_validation_res_imb[l] = f1_score_arr_imb[l].mean(axis=0)
+        
+    print("imbalanced: " + str(cross_validation_res_imb))
+    print("balanced: " + str(cross_validation_res))
+    show_roc_curve(tprs,mean_fpr,aucs,"roc_balanced.png")
+    show_roc_curve(tprs_org,mean_fpr_org,aucs_org,"roc_org.png")
+
+def show_roc_curve(tprs,mean_fpr,aucs,filename):
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+         label='Chance', alpha=.8)
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    plt.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig(filename,bbox_inches='tight')
+    plt.show()
 
 # split training data into training and testing for validation  
 def split_data():
@@ -173,21 +252,30 @@ def create_dataframe():
     return traindf, testdf,train_class
 
 
-
-def training(traindf, testdf,train_class):
+# perform pca to reduce the dimension, and use SMOTE over sampling to balance data
+def training_sampling(traindf, testdf,train_class):
     # standardize dataset before applying PCA 
-    scaler = StandardScaler()
-    train_std = scaler.fit_transform(traindf) 
-    test_std = scaler.transform(testdf)
+    # scaler = StandardScaler()
+    # train_std = scaler.fit_transform(traindf) 
+    # test_std = scaler.transform(testdf)
  
-    train_red, test_red = pca_reduction(train_std,test_std)
+    train_red, test_red = pca_reduction(traindf,testdf)
 
     # Combine over- and under-sampling using SMOTE and Tomek links.
-    smote_tomek = SMOTETomek(random_state=42)
-    train_resampled, class_resampled = smote_tomek.fit_resample(train_red,train_class)
+    # smote_tomek = SMOTETomek(random_state=42)
+    # train_resampled, class_resampled = smote_tomek.fit_resample(train_red,train_class)
+    smote = SMOTE(random_state=42)
+    train_resampled, class_resampled = smote.fit_resample(train_red,train_class)
     return test_red, train_resampled, class_resampled
 
+# train the original data without balancing
+def training(traindf, testdf,train_class):
+    # standardize dataset before applying PCA 
+ 
+    train_red, test_red = pca_reduction(traindf,testdf)
+    return test_red, train_red, train_class
 
+# pca, set the lambda = 0.85
 def pca_reduction(train_std,test_std):
 
     #Plotting the Cumulative Summation of the Explained Variance
@@ -199,36 +287,71 @@ def pca_reduction(train_std,test_std):
     # plt.title('Drug Dataset PCA Explained Variance')
     # plt.show()
     # choose the minimum number of principal components such that 99% of the variance is retained.
+
+    # pca
     pca = PCA(pca_lambda)
     train_pca = pca.fit_transform(train_std)
     test_pca = pca.transform(test_std)
     print(train_pca.shape)
+
+    # svd
+    # svd = TruncatedSVD(n_components=400, n_iter=7, random_state=42)
+    # train_pca = svd.fit_transform(train_std)
+    # test_pca = svd.transform(test_std)
+    # print(train_pca.shape)
+
     return train_pca,test_pca
 
 
 
 def SVM_classifier(train_resampled,class_resampled,test_red,test_class):
-    classifier = SVC(kernel='linear')
-    classifier.fit(train_resampled,class_resampled)
+    svm_cls = SVC(kernel='linear',probability=True)
+    svm_cls.fit(train_resampled,class_resampled)
+    # svm_adaboost = AdaBoostClassifier(svm_cls,n_estimators=100,random_state=42)
+    # svm_adaboost.fit(train_resampled, class_resampled)
+
     # predict
-    predict = classifier.predict(test_red)
+    predict = svm_cls.predict(test_red)
+    # predict = svm_adaboost.predict(test_red)
     # for i in range(len(predict)):
     #     print(predict[i])
-    print("SVM_classifier:\n")
-    print("pca lambda = " + str(pca_lambda))
-    f1_score = evaluation(test_class,predict)
-    return f1_score
-
-def random_forest_classifier(train_resampled,class_resampled,test_red,test_class):
-    f1_score =0.0
-    predict = RandomForestClassifier (n_estimators=num_trees, random_state=0,min_samples_leaf=50).fit(train_resampled, class_resampled).predict(test_red) 
-    # for i in range(TEST_SIZE):
-    #     print(predict[i])
-    print("random forest classifier classifier:\n"+ "number of trees = "+ str(num_trees))
+    print("svm_adaboost:")
     print("pca lambda = " + str(pca_lambda))
     f1_score = evaluation(test_class,predict)
     print(f1_score)
     return f1_score
+
+def random_forest_classifier(train_resampled,class_resampled,test_red,test_class):
+    f1_score =0.0
+    # grid search
+    # param_dist = {"criterion": ["gini", "entropy"],"min_samples_split": [10,20,50],
+                #   "max_depth": [5,6,7,8,9,10],"min_samples_leaf": [10,15,20],"max_leaf_nodes": [30,40,50]}
+    
+    # rf_grid_search = GridSearchCV(rand_forest,param_grid=param_dist, cv=10,scoring='f1')
+    # rf_grid_search.fit(train_resampled,class_resampled)
+    # predict = rf_grid_search.predict(test_red)
+
+    # random cv
+    # rand_forest = RandomForestClassifier(class_weight={0:1,1:2})
+    param_dist = {"criterion": ["gini", "entropy"],"min_samples_split": randint(2, 50),
+                  "max_depth": randint(1, 10),"min_samples_leaf": randint(10, 60),"max_leaf_nodes": randint(10,50), 'n_estimators': randint(50,200)}
+    rand_forest = RandomForestClassifier(criterion='entropy',max_depth = 8,max_leaf_nodes = 31, min_samples_leaf =12,min_samples_split = 26, n_estimators=num_trees)
+    # rand_forest_rs = RandomizedSearchCV(rand_forest, param_distributions=param_dist, cv=10,scoring='f1',n_iter=20)
+    # rand_forest_rs.fit(train_resampled, class_resampled)
+    # predict = rand_forest_rs.predict(test_red)
+    rand_forest.fit(train_resampled, class_resampled)
+    probas_ = rand_forest.predict_proba(test_red)
+    predict = rand_forest.predict(test_red)
+    # predict = RandomForestClassifier (n_estimators=num_trees, random_state=0,min_samples_leaf=50).fit(train_resampled, class_resampled).predict(test_red) 
+    for i in range(len(predict)):
+        print(predict[i])
+    print("random forest classifier classifier:" + " number of tree = " + str(num_trees))
+    print("pca lambda = " + str(pca_lambda))
+    # f1_score = evaluation(test_class,predict)
+    # print(f1_score)
+    # print(rand_forest_rs.best_params_)
+    # print(rand_forest_rs.best_score_)
+    return f1_score, probas_
     
 def decision_tree_classifier(train_resampled,class_resampled,test_red,test_class):
    
@@ -259,17 +382,51 @@ def naive_bayes_classifier(train_resampled,class_resampled,test_red,test_class):
 def adaboost_classifier(train_resampled,class_resampled,test_red,test_class):
     f1_score =0.0
     # predict
-    predict = AdaBoostClassifier(random_state=0).fit(train_resampled, class_resampled).predict(test_red) 
+    param_dist = {"criterion": ["gini", "entropy"],"min_samples_split": randint(2, 50),
+                  "max_depth": randint(1, 20),"min_samples_leaf": randint(2, 15),"max_leaf_nodes": randint(2,50)}
+    # param_dist = {"criterion": ["gini", "entropy"],"min_samples_split": [10,20,50],
+    #               "max_depth": [None,2,5,10],"min_samples_leaf": [2,5,10],"max_leaf_nodes": [None,2,5,10]}
+    clf = tree.DecisionTreeClassifier()
+    # dt_grid_search = GridSearchCV(clf,param_grid=param_dist, cv=10,scoring='f1')
+    dt_randcv = RandomizedSearchCV(clf, param_distributions=param_dist, cv=10,scoring='f1',n_iter=20)
+    dt_randcv.fit(train_resampled, class_resampled)
+    best_clf = dt_randcv.best_estimator_
+    adaboost = AdaBoostClassifier(best_clf,n_estimators=100,random_state=42)
+    adaboost.fit(train_resampled, class_resampled)
+    predict = adaboost.predict(test_red)
+    # predict = AdaBoostClassifier(random_state=0).fit(train_resampled, class_resampled).predict(test_red) 
     # print(predict)
     # for i in range(len(predict)):
     #     print(predict[i])
     print("adaboost classifier:\n")
     print("pca lambda = " + str(pca_lambda))
     f1_score = evaluation(test_class,predict)
+    
+    print(f1_score)
+    return f1_score
+
+def gradient_boosting_clasifier(train_resampled,class_resampled,test_red,test_class):
+    print("gradient boosting classifier classifier:\n")
+    print("pca lambda = " + str(pca_lambda))
+    f1_score =0.0
+    param_dist = {"loss": ["deviance", "exponential"],"min_samples_split": randint(2, 50),
+                  "max_depth": randint(1, 10),"min_samples_leaf": randint(10, 60),"max_leaf_nodes": randint(10,50), 'n_estimators': randint(50,200)}
+    grad_boosting = GradientBoostingClassifier()
+    grad_boosting_rs = RandomizedSearchCV(grad_boosting, param_distributions=param_dist, cv=10,scoring='f1',n_iter=50)
+    grad_boosting_rs.fit(train_resampled, class_resampled)
+    predict = grad_boosting_rs.predict(test_red)
+    # predict = RandomForestClassifier (n_estimators=num_trees, random_state=0,min_samples_leaf=50).fit(train_resampled, class_resampled).predict(test_red) 
+    for i in range(len(predict)):
+        print(predict[i])
+    
+    f1_score = evaluation(test_class,predict)
+    print(f1_score)
+    print(grad_boosting_rs.best_params_)
     return f1_score
 
 def evaluation(test_class,predict):
     # print(classification_report(test_class,predict))
-    return f1_score(test_class, predict,labels = [0,1],average=None) 
+    f1_scores = f1_score(test_class, predict,labels = [0,1],average=None)
+    return  f1_scores
   
 main()
